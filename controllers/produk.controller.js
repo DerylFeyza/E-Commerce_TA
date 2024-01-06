e644ea99b4501c7af372ec470da4a951864afc7f
DerylFeyza <muhammadderylfeyza@gmail.com>
2024-01-06 20:41:01 +0700

    product crud api

--- controllers/produk.controller.js
+++ controllers/produk.controller.js
@@ -4,6 +4,15 @@ const upload = require("./upload-foto").single("gambar_barang");
 const path = require("path");
 const fs = require("fs");
 
+exports.getallProduct = async (request, response) => {
+	let products = await produkModel.findAll();
+	return response.json({
+		success: true,
+		data: products,
+		message: "all products has been loaded",
+	});
+};
+
 exports.addProduct = (request, response) => {
 	upload(request, response, async (error) => {
 		if (error) {
@@ -40,37 +49,67 @@ exports.addProduct = (request, response) => {
 	});
 };
 
-// exports.adduser = (request, response) => {
-// 	upload(request, response, async (error) => {
-// 		if (error) {
-// 			return response.json({ message: error });
-// 		}
-// 		if (!request.file) {
-// 			return response.json({ message: "file nya dimana" });
-// 		}
+exports.updateProduct = async (request, response) => {
+	upload(request, response, async (error) => {
+		if (error) {
+			return response.json({ message: error });
+		}
+		let id = request.params.id;
+		let dataproduk = {
+			nama_barang: request.body.nama_barang,
+			gambar_barang: request.file.filename,
+			kategori: request.body.kategori,
+			harga: request.body.harga,
+			stok: request.body.stok,
+		};
+		if (request.file) {
+			const selectedPhoto = await produkModel.findOne({
+				where: { id: id },
+			});
+			const oldPhoto = selectedPhoto.gambar_barang;
+			const pathPhoto = path.join(__dirname, `../fotoproduk`, oldPhoto);
+			if (fs.existsSync(pathPhoto)) {
+				fs.unlinkSync(pathPhoto, (error) => console.log(error));
+			}
+			dataproduk.foto = request.file.filename;
+		}
+		produkModel
+			.update(dataproduk, { where: { id: id } })
+			.then((result) => {
+				return response.json({
+					success: true,
+					message: "product has been updated",
+				});
+			})
+			.catch((error) => {
+				return response.json({
+					success: false,
+					message: error.message,
+				});
+			});
+	});
+};
 
-// 		let Newuser = {
-// 			nama_user: request.body.nama_user,
-// 			foto: request.file.filename,
-// 			email: request.body.email,
-// 			password: md5(request.body.password),
-// 			role: request.body.role,
-// 		};
-
-// 		userModel
-// 			.create(Newuser)
-// 			.then((result) => {
-// 				return response.json({
-// 					success: true,
-// 					data: result,
-// 					message: "ippe user baru",
-// 				});
-// 			})
-// 			.catch((error) => {
-// 				return response.json({
-// 					success: false,
-// 					message: error.message,
-// 				});
-// 			});
-// 	});
-// };
+exports.deleteProduct = async (request, response) => {
+	const id = request.params.id;
+	const fotodata = await produkModel.findOne({ where: { id: id } });
+	const oldPhoto = fotodata.gambar_barang;
+	const pathPhoto = path.join(__dirname, `../fotoproduk`, oldPhoto);
+	if (fs.existsSync(pathPhoto)) {
+		fs.unlink(pathPhoto, (error) => console.log(error));
+	}
+	produkModel
+		.destroy({ where: { id: id } })
+		.then((result) => {
+			return response.json({
+				success: true,
+				message: "product has been deleted",
+			});
+		})
+		.catch((error) => {
+			return response.json({
+				success: false,
+				message: error.message,
+			});
+		});
+};
--- routes/produk.route.js
+++ routes/produk.route.js
@@ -2,6 +2,31 @@ const express = require("express");
 const app = express();
 const upload = require(`../controllers/upload-foto`);
 const produkController = require("../controllers/produk.controller");
+const auth = require("../auth/auth");
+const { checkRole } = require("../middleware/checkRole");
+app.get(
+	"/",
+	auth.authVerify,
+	checkRole(["admin"]),
+	produkController.getallProduct
+);
+app.post(
+	"/",
+	auth.authVerify,
+	checkRole(["seller", "admin"]),
+	produkController.addProduct
+);
+app.delete(
+	"/delete/:id",
+	auth.authVerify,
+	checkRole(["seller", "admin"]),
+	produkController.deleteProduct
+);
+app.put(
+	"/update/:id",
+	auth.authVerify,
+	checkRole(["seller", "admin"]),
+	produkController.updateProduct
+);
 
-app.post("/", produkController.addProduct);
 module.exports = app;
