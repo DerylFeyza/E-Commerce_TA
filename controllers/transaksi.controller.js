c481389d4391fb237b4474824ab53b7631fe78d7
DerylFeyza <muhammadderylfeyza@gmail.com>
2024-01-09 11:44:34 +0700

    update cart and add cart api

--- controllers/transaksi.controller.js
+++ controllers/transaksi.controller.js
@@ -1,6 +1,6 @@
 const produkModel = require("../models/index").produk;
 const cartModel = require("../models/index").keranjanguser;
-const cartDetailsModel = require("../models/index").keranjangitem;
+const cartDetailsModel = require("../models/index").keranjangitems;
 const Op = require("sequelize").Op;
 
 exports.productToCart = async (request, response) => {
@@ -14,6 +14,37 @@ exports.productToCart = async (request, response) => {
 		where: { id_user: iduser, status: "draft" },
 	});
 	if (checkExistingCart) {
+		const id_keranjang = checkExistingCart.id;
+		// cartModel
+		// 	.update(datapemesanan, { where: { id: idpemesanan } })
+		// 	.then((result) => {
+		cartDetailsModel.destroy({ where: { id_keranjang: id_keranjang } });
+		let detailsoforder = request.body.detailsoforder;
+		for (let i = 0; i < detailsoforder.length; i++) {
+			detailsoforder[i].id_keranjang = id_keranjang;
+		}
+
+		cartDetailsModel
+			.bulkCreate(detailsoforder)
+			.then((result) => {
+				return response.json({
+					success: true,
+					message: "cart has been updated successfully",
+				});
+			})
+			.catch((error) => {
+				return response.json({
+					success: false,
+					message: error.message,
+				});
+			});
+		// })
+		// .catch((error) => {
+		// 	return response.json({
+		// 		success: false,
+		// 		message: error.message,
+		// 	});
+		// });
 	} else {
 		cartModel
 			.create(cartData)
