919564edc8770d9db2c604ee149bc2ba562b563f
DerylFeyza <muhammadderylfeyza@gmail.com>
2024-02-21 14:18:15 +0700

    cors cookie worked

--- server.js
+++ server.js
@@ -5,7 +5,12 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
-app.use(cors());
+const corsOptions = {
+	origin: "http://localhost:5173",
+	credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
